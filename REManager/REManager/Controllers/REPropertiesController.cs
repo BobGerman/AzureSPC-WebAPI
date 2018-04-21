d80473e615723e712e95017c0e270e6903c377fe
Bob German <bgerman1@hotmail.com>
2018-04-21 15:39:41 -0400

    Added Authorize attribute

--- REManager/REManager/Controllers/REPropertiesController.cs
+++ REManager/REManager/Controllers/REPropertiesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using REManager.Models;
 
 namespace REManager.Controllers
 {
+    [Authorize]
     [Produces("application/json")]
     [Route("api/REProperties")]
     public class REPropertiesController : Controller
