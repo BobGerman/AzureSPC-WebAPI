7414ed9dc9ee0d33fd95ad3534f28ce7cf959150
Bob German <bgerman1@hotmail.com>
2018-04-21 15:12:50 -0400

    Initial migration

---
+++ REManager/REManager/Migrations/20180421191051_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace REManager.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReProperties",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Address = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true),
+                    IsForRent = table.Column<bool>(nullable: false),
+                    IsForSale = table.Column<bool>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    PostalCode = table.Column<string>(nullable: true),
+                    PurchaseAmount = table.Column<int>(nullable: true),
+                    PurchaseDate = table.Column<DateTime>(nullable: false),
+                    State = table.Column<string>(nullable: true),
+                    Unit = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReProperties", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReProperties");
+        }
+    }
+}
