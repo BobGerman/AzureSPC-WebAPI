27748867346e1dcadfa41c96f008378629e24c92
Bob German <bgerman1@hotmail.com>
2018-04-21 14:50:16 -0400

    Added DB context

---
+++ REManager/REManager/DataAccess/REPropertyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using REManager.Models;
+
+namespace REManager.DataAccess
+{
+    public class REPropertyDbContext : DbContext
+    {
+        public REPropertyDbContext(DbContextOptions<REPropertyDbContext> options)
+            :base (options) { }
+
+        public DbSet<REProperty> ReProperties { get; set; }
+    }
+}
--- REManager/REManager/Startup.cs
+++ REManager/REManager/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using REManager.DataAccess;
 
 namespace REManager
 {
@@ -25,6 +27,9 @@ namespace REManager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = this.Configuration.GetConnectionString("ReMgrSQL");
+            services.AddDbContext<REPropertyDbContext>(options => options.UseSqlServer(connectionString));
+
             services.AddAuthentication(sharedOptions =>
             {
                 sharedOptions.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
