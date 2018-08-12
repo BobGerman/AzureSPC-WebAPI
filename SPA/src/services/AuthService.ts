7cf7c395e6d9ad940faf20a7d5f58c52b1040856
Bob German <bgerman1@hotmail.com>
2018-08-12 18:37:59 -0400

    Adding login code, loops right now

--- SPA/src/services/AuthService.ts
+++ SPA/src/services/AuthService.ts
@@ -1,48 +1,71 @@
 import * as AuthenticationContext from 'adal-angular';
 
-// TODO: Make it dynamic, not hard-coded values
-const config: AuthenticationContext.Options =
-{
-    tenant: 'a25d4ef1-c73a-4dc1-bdb1-9a342260f216',
-    clientId: '82deab78-7ff2-4e90-baec-83206f937e50',
-    endpoints: {
-        'reMgr': 'https://remanagerws.azurewebsites.net'
-    },
-    cacheLocation: 'localStorage'
-};
-
-// TODO:
-// ??? WHY did articles say I needed this ???
-// Hack to get a reference to ADAL, which is not CommonJS friendly
-// declare const window: any;
-// let adal: AuthenticationContext = window.AuthenticationContext;
 
 export default class AuthService {
 
     private authContext: AuthenticationContext = null;
+    private config: AuthenticationContext.Options = null;
+
+    // TODO: Pass in tenant, clientId, and endpoints collection
+    constructor() {
+
+        this.config = {
+            tenant: 'a25d4ef1-c73a-4dc1-bdb1-9a342260f216',
+            clientId: '82deab78-7ff2-4e90-baec-83206f937e50',
+            endpoints: {
+                'reMgr': 'https://remanagerws.azurewebsites.net'
+            },
+            redirectUri: 'http://localhost:8080',
+            cacheLocation: 'localStorage'
+        };
+        this.authContext = new AuthenticationContext(this.config);
+
+    }
 
     // Pass client id etc here:
     public getToken(): Promise<string> {
+
         return new Promise<string>((resolve, reject) => {
 
-            const endpoint = config.endpoints['reMgr'];
-            this.authContext = new AuthenticationContext(config);
+            if (this.ensureLogin()) {
+                const endpoint = this.config.endpoints['reMgr'];
 
-            let cachedToken = this.authContext.getCachedToken(endpoint);
-            if (cachedToken) {
-                resolve(cachedToken);
-            } else {
-                this.authContext.acquireToken(
-                    endpoint,
-                    (error, acquiredToken) => {
-                        if (error || !acquiredToken) {
-                            reject(error);
-                        } else {
-                            resolve(acquiredToken);
+                let cachedToken = this.authContext.getCachedToken(endpoint);
+                if (cachedToken) {
+                    resolve(cachedToken);
+                } else {
+                    this.authContext.acquireToken(
+                        endpoint,
+                        (error, acquiredToken) => {
+                            if (error || !acquiredToken) {
+                                reject(error);
+                            } else {
+                                resolve(acquiredToken);
+                            }
                         }
-                    }
-                )
+                    );
+                }
+            } else {
+                reject(`Login error: ${this.authContext.getLoginError()}`);
             }
-        })
+        });
+
     }
+
+    private ensureLogin(): boolean {
+
+        var isCallback = this.authContext.isCallback(window.location.hash);
+        if (isCallback && this.authContext.getLoginError()) {
+            this.authContext.handleWindowCallback(window.location.hash);
+        } else {
+            var user = this.authContext.getCachedUser();
+            if (!user) {
+                this.authContext.login();
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
